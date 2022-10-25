f4317531296ac240484f82db0ff48f0144c5db42
Ashraful Alam <ashrafcse3@gmail.com>
2022-10-25 21:05:57 +0100

    added courses title api

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
+const coursesTitle = require('./data/coursesTitles.json');
+
 app.get('/', (req, res) => {
     res.send('nodemon later from express / route');
+})
+
+app.get('/courses-title', (req, res) => {
+    res.send(coursesTitle);
 })
 
 app.listen(port, () => {
