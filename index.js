c394a5ea4ea0ab692ebe3e84614157c0db073805
Ashraful Alam <ashrafcse3@gmail.com>
2022-10-25 21:07:42 +0100

    courses details api added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const coursesTitle = require('./data/coursesTitles.json');
+const coursesDetails = require('./data/coursesDetails.json');
 
 app.get('/', (req, res) => {
     res.send('nodemon later from express / route');
@@ -10,6 +11,10 @@ app.get('/', (req, res) => {
 
 app.get('/courses-title', (req, res) => {
     res.send(coursesTitle);
+})
+
+app.get('/courses-details', (req, res) => {
+    res.send(coursesDetails);
 })
 
 app.listen(port, () => {
