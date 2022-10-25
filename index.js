6338b8c34f848d80a306148cd116a1f99eeb951b
Ashraful Alam <ashrafcse3@gmail.com>
2022-10-25 21:24:43 +0100

    added cors middleware

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use(cors());
 
 const coursesTitle = require('./data/coursesTitles.json');
 const coursesDetails = require('./data/coursesDetails.json');
